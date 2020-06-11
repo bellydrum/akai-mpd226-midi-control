0dddb9c68a831c29b339651a8178639fc4bec050
bellydrum <davidmaness1990@gmail.com>
2020-06-10 21:27:48 -0500

    custom presets work now

--- Akai MPD226/classes/MPD226.py
+++ Akai MPD226/classes/MPD226.py
@@ -2,22 +2,22 @@ from .MPD226Input import *
 
 class MPD226:
 
-    pad_1 = Pad(1, 'pad', 37)
-    pad_2 = Pad(2, 'pad', 36)
-    pad_3 = Pad(3, 'pad', 42)
-    pad_4 = Pad(4, 'pad', 82)
-    pad_5 = Pad(5, 'pad', 40)
-    pad_6 = Pad(6, 'pad', 38)
-    pad_7 = Pad(7, 'pad', 46)
-    pad_8 = Pad(8, 'pad', 44)
-    pad_9 = Pad(9, 'pad', 48)
-    pad_10 = Pad(10, 'pad', 47)
-    pad_11 = Pad(11, 'pad', 45)
-    pad_12 = Pad(12, 'pad', 43)
-    pad_13 = Pad(13, 'pad', 49)
-    pad_14 = Pad(14, 'pad', 55)
-    pad_15 = Pad(15, 'pad', 51)
-    pad_16 = Pad(16, 'pad', 53)
+    pad_1 = Pad(1, 'pad', 37)  # C#2
+    pad_2 = Pad(2, 'pad', 36)  # C2
+    pad_3 = Pad(3, 'pad', 42)  # F#2
+    pad_4 = Pad(4, 'pad', 82)  # A#5
+    pad_5 = Pad(5, 'pad', 40)  # E2
+    pad_6 = Pad(6, 'pad', 38)  # D2
+    pad_7 = Pad(7, 'pad', 46)  # A#2
+    pad_8 = Pad(8, 'pad', 44)  # G#2
+    pad_9 = Pad(9, 'pad', 48)  # C3
+    pad_10 = Pad(10, 'pad', 47)  # B2
+    pad_11 = Pad(11, 'pad', 45)  # A2
+    pad_12 = Pad(12, 'pad', 43)  # G2
+    pad_13 = Pad(13, 'pad', 49)  # C#3
+    pad_14 = Pad(14, 'pad', 55)  # G3
+    pad_15 = Pad(15, 'pad', 51)  # D#3
+    pad_16 = Pad(16, 'pad', 53)  # F3
     knob_1 = Knob(1, 'knob', 3)
     knob_2 = Knob(2, 'knob', 9)
     knob_3 = Knob(3, 'knob', 14)
@@ -43,10 +43,15 @@ class MPD226:
     def __init__(self):
         self.events = {
             153: 'Note On',
+            144: 'Note On',  # custom preset value
             137: 'Note Off',
+            128: 'Note Off',  # custom preset value
             176: 'Control Change',
+            185: 'Control Change',  # custom preset value
             169: 'Channel Aftertouch (Poly)',
-            217: 'Channel Aftertouch (Channel)'
+            160: 'Channel Aftertouch (Poly)',  # custom preset value
+            217: 'Channel Aftertouch (Channel)',
+            208: 'Channel Aftertouch (Channel)'  # custom preset value
         }
         self.pads_by_id = {
             37: self.pad_1,
