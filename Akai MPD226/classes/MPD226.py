aa37b5341f6520db7d0321c14dd57fbad9f2f9e8
Randall Ward <randall.ward@mindbodyonline.com>
2020-10-05 14:03:53 -0500

    Add files via upload

--- Akai MPD226/classes/MPD226.py
+++ Akai MPD226/classes/MPD226.py
@@ -2,22 +2,22 @@ from .MPD226Input import *
 
 class MPD226:
 
-    pad_1 = Pad(1, 'pad', 36)  # C#2
-    pad_2 = Pad(2, 'pad', 37)  # C2
-    pad_3 = Pad(3, 'pad', 38)  # F#2
-    pad_4 = Pad(4, 'pad', 39)  # A#5
+    pad_1 = Pad(1, 'pad', 37)  # C#2
+    pad_2 = Pad(2, 'pad', 36)  # C2
+    pad_3 = Pad(3, 'pad', 42)  # F#2
+    pad_4 = Pad(4, 'pad', 82)  # A#5
     pad_5 = Pad(5, 'pad', 40)  # E2
-    pad_6 = Pad(6, 'pad', 41)  # D2
-    pad_7 = Pad(7, 'pad', 42)  # A#2
-    pad_8 = Pad(8, 'pad', 43)  # G#2
-    pad_9 = Pad(9, 'pad', 44)  # C3
-    pad_10 = Pad(10, 'pad', 45)  # B2
-    pad_11 = Pad(11, 'pad', 46)  # A2
-    pad_12 = Pad(12, 'pad', 47)  # G2
-    pad_13 = Pad(13, 'pad', 48)  # C#3
-    pad_14 = Pad(14, 'pad', 49)  # G3
-    pad_15 = Pad(15, 'pad', 50)  # D#3
-    pad_16 = Pad(16, 'pad', 51)  # F3
+    pad_6 = Pad(6, 'pad', 38)  # D2
+    pad_7 = Pad(7, 'pad', 46)  # A#2
+    pad_8 = Pad(8, 'pad', 44)  # G#2
+    pad_9 = Pad(9, 'pad', 48)  # C3
+    pad_10 = Pad(10, 'pad', 47)  # B2
+    pad_11 = Pad(11, 'pad', 45)  # A2
+    pad_12 = Pad(12, 'pad', 43)  # G2
+    pad_13 = Pad(13, 'pad', 49)  # C#3
+    pad_14 = Pad(14, 'pad', 55)  # G3
+    pad_15 = Pad(15, 'pad', 51)  # D#3
+    pad_16 = Pad(16, 'pad', 53)  # F3
     knob_1 = Knob(1, 'knob', 3)
     knob_2 = Knob(2, 'knob', 9)
     knob_3 = Knob(3, 'knob', 14)
@@ -54,22 +54,22 @@ class MPD226:
             208: 'Channel Aftertouch (Channel)'  # custom preset value
         }
         self.pads_by_id = {
-            36: self.pad_1,
-            37: self.pad_2,
-            38: self.pad_3,
-            39: self.pad_4,
+            37: self.pad_1,
+            36: self.pad_2,
+            42: self.pad_3,
+            82: self.pad_4,
             40: self.pad_5,
-            41: self.pad_6,
-            42: self.pad_7,
-            43: self.pad_8,
-            44: self.pad_9,
-            45: self.pad_10,
-            46: self.pad_11,
-            47: self.pad_12,
-            48: self.pad_13,
-            49: self.pad_14,
-            50: self.pad_15,
-            51: self.pad_16
+            38: self.pad_6,
+            46: self.pad_7,
+            44: self.pad_8,
+            48: self.pad_9,
+            47: self.pad_10,
+            45: self.pad_11,
+            43: self.pad_12,
+            49: self.pad_13,
+            55: self.pad_14,
+            51: self.pad_15,
+            53: self.pad_16
         }
         self.knobs_by_id = {
             3: self.knob_1,
